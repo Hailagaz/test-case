9d442b451d895f53d957be0b38dc02f249f2ac9d
Hailagaz <omentielvo.lumenn@gmail.com>
2023-03-17 13:12:37 +0200

    fix

--- script.js
+++ script.js
@@ -7,7 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
 		hider.classList.toggle("change");
 		section1.classList.toggle("hided");
 		section2.classList.toggle("hided");
-	})
+	});
+
+
+
+
+
+	const phoneInputField = document.querySelector("#phone");
+	const phoneInput = window.intlTelInput(phoneInputField, {
+		preferredCountries: ["ua", "us", "pl", "de"],
+		utilsScript:
+			"https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+	});
+
+
+	const info = document.querySelector(".alert-info");
+	function process(event) {
+		event.preventDefault();
+		const phoneNumber = phoneInput.getNumber();
+		info.style.display = "";
+		info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+	}
+
+
+
+
 });
 
 
