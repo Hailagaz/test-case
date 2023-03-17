269a0a89eeb5f490368bf58a9fb8f8c2114df69a
Hailagaz <omentielvo.lumenn@gmail.com>
2023-03-17 12:24:43 +0200

    fix

--- script.js
+++ script.js
@@ -8,24 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		section1.classList.toggle("hided");
 		section2.classList.toggle("hided");
 	})
-
-
-
-	// hider.onclick = function () {
-	// 	hider.classList.toggle("change");
-	// 	if (!section1.hidden) {
-	// 		section1.hidden = true;
-	// 	} else {
-	// 		section1.hidden = false;
-	// 	}
-	// 	section1.hidden = true;
-	// 	section2.hidden = true;
-	// }
-
-	// hider.onclick = function () {
-	// 	hider.classList.remove("change");
-	// 	document.getElementById('solution').hidden = false;
-	// 	document.getElementById('start').hidden = false;
-	// }
 });
+
 
