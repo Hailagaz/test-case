0642bc1f484b277dea8b5f7edafc2332b3aad6c0
Hailagaz <omentielvo.lumenn@gmail.com>
2023-03-17 15:29:14 +0200

    fix

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+
+$(function () {
+	$(".video").click(function () {
+		var theModal = $(this).data("target"),
+			videoSRC = $(this).attr("data-video"),
+			videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=1&showinfo=1&html5=1&autoplay=1&mute=1";
+		$(theModal + ' iframe').attr('src', videoSRCauto);
+		$(theModal + ' button.close').click(function () {
+			$(theModal + ' iframe').attr('src', videoSRC);
+		});
+	});
+});
